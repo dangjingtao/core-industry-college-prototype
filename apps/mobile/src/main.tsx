d92dbac5db040868e6f3aecb9b915b4b2a4bc4b1
DangJingtao <dangjingtao@163.com>
2026-09-03 02:16:34 +0800

    style(mobile): load home visual review fixes

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app/App";
 import "./styles.css";
+import "./home-polish-fixes.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}><App /></BrowserRouter></React.StrictMode>,
